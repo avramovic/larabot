df5f1d8b0baa4badfa1a9b04617ad13738fe865b
Nemanja <nemanja.avramovic@partech.com>
2026-02-13 01:17:33 +0100

    fix settings

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -18,19 +18,8 @@ class Setting extends Model
 {
     protected $guarded = [];
 
-    public static Collection $settings;
-
-    protected static function loadSettings(): void
-    {
-        static::$settings = static::all();
-    }
-
     public static function get(string $key, mixed $default = null): mixed
     {
-        if (empty(static::$settings)) {
-            static::loadSettings();
-        }
-
         $setting = static::$settings->firstWhere('key', $key);
         if (!$setting) {
             return $default;
@@ -47,17 +36,9 @@ class Setting extends Model
 
     public static function set(string $key, mixed $value, ?string $type = null): Setting
     {
-        $setting = Setting::updateOrCreate(
+        return Setting::updateOrCreate(
             ['key' => $key],
             ['value' => is_array($value) ? json_encode($value) : (string) $value, 'type' => $type ?? get_debug_type($value)]
         );
-        if ($old_setting = static::$settings->firstWhere('key', $key)) {
-            $old_setting->value = $setting->value;
-            $old_setting->type = $setting->type;
-        } else {
-            static::$settings->push($setting);
-        }
-
-        return $setting;
     }
 }
